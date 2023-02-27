615294ce937586d3d56b81b61750793e7cbd3275
krsnamara <martinj.fitzpatrick@gmail.com>
2023-02-27 11:25:34 -0500

    updated brand statement

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -25,7 +25,7 @@
             <div class="container-two">
                 <div class="about-me">
                     <h1>About Me</h1>
-                    <p>I'm baby flexitarian DIY schlitz four dollar toast you probably haven't heard of them, flannel cred listicle organic. Roof party mumblecore viral 3 wolf moon. Aesthetic chillwave cronut hoodie mustache semiotics tbh. Bodega boys leggings 8-bit raw denim flannel, affogato chicharrones pug scenester sustainable kitsch health goth master cleanse.</p>
+                    <p>Experienced in JavaScript and Python based programming, with a background in construction and property maintenance in Brooklyn, NY. Possess strong skills in communication, function, and project management that help tech companies build beautiful, responsive applications. Committed empathetic sociable developer with a passion for creating positive energy towards goals and projects. Seeking a challenging work ethic that produces useful and well thought out solutions for everyday tasks. Efficient with time allocation and ability to break down larger projects into smaller goals to achieve great ends.</p>
                 </div>
                 <div class="technology-section">
                     <h1>Technologies Studied</h1>
